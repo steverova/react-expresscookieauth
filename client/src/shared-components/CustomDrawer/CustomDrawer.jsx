780a1370074fb009b6b1e294ac8eb84d3ebf42cf
steveMM <steven.rojas@movilmultimediasa.com>
2024-08-16 21:44:19 -0600

    chore: Add lazy loading for images in DataTable view

---
+++ client/src/shared-components/CustomDrawer/CustomDrawer.jsx
@@ -0,0 +1,53 @@
+/* eslint-disable react/prop-types */
+import { Box, Container, Drawer, IconButton } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+
+function CustomDrawer({
+  children,
+  open = false,
+  closeHandler,
+  size = "70%",
+  anchor = "right",
+}) {
+  return (
+    <Drawer
+      PaperProps={{
+        sx: {
+          width: { sm: "100%", md: size },
+          display: "flex",
+          flexDirection: "column",
+          borderLeft: "1rem solid #2c7a7b",
+        },
+      }}
+      anchor={anchor}
+      open={open}
+    >
+      <Box style={{ maxHeight: "100vh" }}>
+        <Box className=" justify-end flex" >
+          <IconButton
+            variant="contained"
+            color="primary"
+            onClick={closeHandler}
+            style={{ marginTop: "auto" }}
+          >
+            <CloseIcon />
+          </IconButton>
+        </Box>
+
+        <Container
+          style={{ height: "90vh" }}
+          maxWidth="lg"
+          className=" overflow-y-auto"
+        >
+          {children}
+        </Container>
+      </Box>
+    </Drawer>
+  );
+}
+
+CustomDrawer.defaultProps = {
+  size: "70%",
+};
+
+export default CustomDrawer;
--- client/src/views/DataTable.jsx
+++ client/src/views/DataTable.jsx
@@ -1,45 +1,51 @@
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import Button from "@mui/material/Button";
-
-const columns = [
-  { field: "id", headerName: "ID", flex: 1 },
-  { field: "firstName", headerName: "First name", flex: 1 },
-  { field: "lastName", headerName: "Last name", flex: 1 },
-
-  {
-    field: "Actions",
-    headerName: "Actions",
-    description: "actions for...",
-    sortable: false,
-    flex: 1,
-    renderCell: (params) => (
-      <Button
-        variant="contained"
-        size="small"
-        style={{ marginLeft: 16 }}
-        tabIndex={params.hasFocus ? 0 : -1}
-      >
-        Open
-      </Button>
-    ),
-  },
-];
-
-const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-];
+import useDisclousure from "../hooks/useDisclousure";
+import CustomDrawer from "../shared-components/CustomDrawer/CustomDrawer";
 
 export default function DataTable() {
+  const { handleCloseComponent, handleOpenComponent, open } = useDisclousure();
+  const columns = [
+    { field: "id", headerName: "ID", flex: 1 },
+    { field: "firstName", headerName: "First name", flex: 1 },
+    { field: "lastName", headerName: "Last name", flex: 1 },
+
+    {
+      field: "Actions",
+      headerName: "Actions",
+      description: "actions for...",
+      sortable: false,
+      flex: 1,
+      renderCell: (params) => (
+        <Button
+          onClick={handleOpenComponent}
+          size="small"
+          style={{ marginLeft: 16 }}
+          tabIndex={params.hasFocus ? 0 : -1}
+        >
+          Open
+        </Button>
+      ),
+    },
+  ];
+
+  const rows = [
+    { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+    { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+    { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+    { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+    { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+    { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+    { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+    { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+  ];
+
   return (
     <div className="p-24" style={{ width: "100%" }}>
+      <CustomDrawer open={open} closeHandler={handleCloseComponent}>
+        drawer
+      </CustomDrawer>
       <DataGrid
         disableColumnFilter
         disableColumnSelector
@@ -55,7 +61,7 @@ export default function DataTable() {
         }}
         initialState={{
           pagination: {
-            paginationModel: { page: 0, pageSize: 5 },
+            paginationModel: { page: 0, pageSize: 10 },
           },
         }}
         pageSizeOptions={[5, 10]}
