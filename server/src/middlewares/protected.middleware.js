a1bd0d9de93ce09a5e25aaa179881752bbc364d4
steveMM <steven.rojas@movilmultimediasa.com>
2024-08-18 19:48:02 -0600

    chore: Update npm dependencies and add Biome config package

--- server/src/middlewares/protected.middleware.js
+++ server/src/middlewares/protected.middleware.js
@@ -1,73 +1,74 @@
-import { StatusCodes } from "http-status-codes";
-import { getJWT } from "../plugins/plugins.js";
-import authHelper from "../helpers/auth.helper.js";
+import { StatusCodes } from "http-status-codes"
+import { getJWT } from "../plugins/plugins.js"
+import authHelper from "../helpers/auth.helper.js"
 
-const authMiddleware = async (req, res, next) => {
-  const token = req.cookies.authcookie;
-  const refreshToken = req.cookies.refreshcookie;
+const authMiddleware = async (req, res) => {
 
-  if (!token && !refreshToken) {
-    return res
-      .status(StatusCodes.UNAUTHORIZED)
-      .send({ message: "NO_TOKENS_PROVIDED", autorized: false, content: [] });
-  }
-  if (!token) {
-    await generateNewToken(req, res);
-    return;
-  }
+	const token = req.cookies.authcookie
+	const refreshToken = req.cookies.refreshcookie
+	if (!token && !refreshToken) {
+		return res
+			.status(StatusCodes.UNAUTHORIZED)
+			.send({ message: "NO_TOKENS_PROVIDED", autorized: false, content: [] })
+	}
 
-  const jwtResponse = getJWT().verify(token, process.env.SECRET_TOKEN_KEY);
+	if (!token) {
+		await generateNewToken(req, res)
+		return
+	}
 
-  if (!jwtResponse) {
-    return res
-      .status(StatusCodes.UNAUTHORIZED)
-      .send({ message: "INVALID_TOKEN", content: [] });
-  }
+	const jwtResponse = getJWT().verify(token, process.env.SECRET_TOKEN_KEY)
 
-  return res
-    .status(StatusCodes.OK)
-    .send({ message: "VALID_TOKEN", autorized: true, content: [] });
-};
+	if (!jwtResponse) {
+		return res
+			.status(StatusCodes.UNAUTHORIZED)
+			.send({ message: "INVALID_TOKEN", content: [] })
+	}
+
+	return res
+		.status(StatusCodes.OK)
+		.send({ message: "VALID_TOKEN", autorized: true, content: [] })
+}
 
 const generateNewToken = async (req, res) => {
-  const refreshToken = req.cookies.refreshcookie;
+	const refreshToken = req.cookies.refreshcookie
 
-  if (!refreshToken) {
-    return res
-      .status(StatusCodes.UNAUTHORIZED)
-      .send({ message: "NO_REFRESH_TOKEN_PROVIDED", content: [] });
-  }
+	if (!refreshToken) {
+		return res
+			.status(StatusCodes.UNAUTHORIZED)
+			.send({ message: "NO_REFRESH_TOKEN_PROVIDED", content: [] })
+	}
 
-  const jwtResponse = getJWT().verify(
-    refreshToken,
-    process.env.SECRET_TOKEN_KEY
-  );
+	const jwtResponse = getJWT().verify(
+		refreshToken,
+		process.env.SECRET_TOKEN_KEY,
+	)
 
-  if (!jwtResponse) {
-    return res
-      .status(StatusCodes.UNAUTHORIZED)
-      .send({ message: "INVALID_REFRESH_TOKEN", content: [] });
-  }
+	if (!jwtResponse) {
+		return res
+			.status(StatusCodes.UNAUTHORIZED)
+			.send({ message: "INVALID_REFRESH_TOKEN", content: [] })
+	}
 
-  const decode = getJWT().decode(refreshToken);
+	const decode = getJWT().decode(refreshToken)
 
-  const token = await authHelper.generateToken(
-    {
-      email: decode.email,
-      name: decode.name,
-    },
-    process.env.COOKIE_EXPIRE_TOKEN_TIME
-  );
+	const token = await authHelper.generateToken(
+		{
+			email: decode.email,
+			name: decode.name,
+		},
+		process.env.COOKIE_EXPIRE_TOKEN_TIME,
+	)
 
-  return res
-    .cookie("authcookie", token, {
-      maxAge: 15 * 60 * 1000,
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
-    })
-    .status(StatusCodes.OK)
-    .send({ message: "VALID_TOKEN", autorized: true, content: [] });
-};
+	return res
+		.cookie("authcookie", token, {
+			maxAge: 15 * 60 * 1000,
+			httpOnly: true,
+			secure: process.env.NODE_ENV === "production",
+			sameSite: "strict",
+		})
+		.status(StatusCodes.OK)
+		.send({ message: "VALID_TOKEN", autorized: true, content: [] })
+}
 
-export default authMiddleware;
+export default authMiddleware
--- server/src/services/auth.service.js
+++ server/src/services/auth.service.js
@@ -1,155 +1,146 @@
-import { StatusCodes } from "http-status-codes";
-import AuthRepository from "../repository/auth.repository.js";
-import fs from "node:fs";
-const __dirname = dirname(fileURLToPath(import.meta.url));
-import authHelper from "../helpers/auth.helper.js";
-import { dirname, join } from "node:path";
-import { fileURLToPath } from "node:url";
+import { StatusCodes } from "http-status-codes"
+import AuthRepository from "../repository/auth.repository.js"
+import fs from "node:fs"
+const __dirname = dirname(fileURLToPath(import.meta.url))
+import authHelper from "../helpers/auth.helper.js"
+import { dirname, join } from "node:path"
+import { fileURLToPath } from "node:url"
 
-const srcDir = join(__dirname, "..");
+const srcDir = join(__dirname, "..")
 
 const AuthService = () => {
-  const auth = AuthRepository();
+	const auth = AuthRepository()
 
-  const login = async (req, res) => {
-    const { email, password } = req.body;
+	const login = async (req, res) => {
+		const { email, password } = req.body
 
-    const verify = await auth.verifyEmail(email);
+		const verify = await auth.verifyEmail(email)
 
-    if (!verify.isFound) {
-      return res
-        .status(StatusCodes.NOT_FOUND)
-        .send({ message: "USER_NOT_FOUND", data: [] });
-    }
+		if (!verify.isFound) {
+			return res
+				.status(StatusCodes.NOT_FOUND)
+				.send({ message: "USER_NOT_FOUND", data: [] })
+		}
 
-    const isPasswordValid = await authHelper.comparePassword(
-      password,
-      verify.data.auth.password
-    );
+		const isPasswordValid = await authHelper.comparePassword(
+			password,
+			verify.data.auth.password,
+		)
 
-    if (!isPasswordValid) {
-      return res
-        .status(StatusCodes.UNAUTHORIZED)
-        .send({ message: "INVALID_PASSWORD", content: [] });
-    }
+		if (!isPasswordValid) {
+			return res
+				.status(StatusCodes.UNAUTHORIZED)
+				.send({ message: "INVALID_PASSWORD", content: [] })
+		}
 
-    const token = await authHelper.generateToken(
-      {
-        email: verify.data.user.email,
-        name: verify.data.user.name,
-      },
-      "15m"
-    );
+		const token = await authHelper.generateToken(
+			{ email: verify.data.user.email, name: verify.data.user.name },
+			"15m",
+		)
 
-    const refreshToken = await authHelper.generateToken(
-      {
-        email: verify.data.user.email,
-        name: verify.data.user.name,
-      },
-      "7h"
-    );
+		const refreshToken = await authHelper.generateToken(
+			{
+				email: verify.data.user.email,
+				name: verify.data.user.name,
+			},
+			"7h",
+		)
 
-    return res
-      .cookie("authcookie", token, {
-        maxAge: 15 * 60 * 1000,
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
-      })
-      .cookie("refreshcookie", refreshToken, {
-        maxAge: 7 * 60 * 60 * 1000,
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
-      })
-      .status(StatusCodes.OK)
-      .send({ message: "LOGIN_SUCCESS", content: verify.data.user });
-  };
+		return res
+			.cookie("authcookie", token, {
+				maxAge: 15 * 60 * 1000,
+				httpOnly: true,
+				secure: process.env.NODE_ENV === "production",
+				sameSite: "strict",
+			})
+			.cookie("refreshcookie", refreshToken, {
+				maxAge: 7 * 60 * 60 * 1000,
+				httpOnly: true,
+				secure: process.env.NODE_ENV === "production",
+				sameSite: "strict",
+			})
+			.status(StatusCodes.OK)
+			.send({ message: "LOGIN_SUCCESS", content: verify.data.user })
+	}
 
-  const logout = async (_, res) => {
-    return res
-      .clearCookie("authcookie")
-      .clearCookie("refreshcookie")
-      .status(StatusCodes.OK)
-      .send({ message: "LOGOUT_SUCCESS", data: [] });
-  };
+	const logout = async (_, res) => {
+		return res
+			.clearCookie("authcookie")
+			.clearCookie("refreshcookie")
+			.status(StatusCodes.OK)
+			.send({ message: "LOGOUT_SUCCESS", data: [] })
+	}
 
-  const register = async (req, res) => {
-    const { name, lastname, email, password, avatar, active } = req.body;
+	const register = async (req, res) => {
+		const { name, lastname, email, password, avatar, active } = req.body
 
-    const verifyResponse = await auth.verifyEmail(email);
+		const verifyResponse = await auth.verifyEmail(email)
 
-    if (verifyResponse.isFound) {
-      return res
-        .status(StatusCodes.BAD_REQUEST)
-        .send({ message: "USER_EXIST", data: [] });
-    }
+		if (verifyResponse.isFound) {
+			return res
+				.status(StatusCodes.BAD_REQUEST)
+				.send({ message: "USER_EXIST", data: [] })
+		}
 
-    const registerResponse = await auth.register({
-      name,
-      lastname,
-      email,
-      password,
-      avatar,
-      active,
-    });
+		const registerResponse = await auth.register({
+			name,
+			lastname,
+			email,
+			password,
+			avatar,
+			active,
+		})
 
-    return res
-      .status(StatusCodes.OK)
-      .send({ message: "USER_CREATED", data: registerResponse });
-  };
+		return res
+			.status(StatusCodes.OK)
+			.send({ message: "USER_CREATED", data: registerResponse })
+	}
 
-  const getPublicKey = async (_, res) => {
-    const publicKeyPath = `${srcDir}/helpers/id_rsa_public.pem`;
-    const publicKey = fs.readFileSync(publicKeyPath, "utf8");
-    return res.status(StatusCodes.OK).send({ publicKey });
-  };
+	const getPublicKey = async (_, res) => {
+		const publicKeyPath = `${srcDir}/helpers/id_rsa_public.pem`
+		const publicKey = fs.readFileSync(publicKeyPath, "utf8")
+		return res.status(StatusCodes.OK).send({ publicKey })
+	}
 
-  // const validateAuthCookies = async (_, res) => {
-  //   return res
-  //     .status(StatusCodes.OK)
-  //     .send({ message: "VALID_TOKEN", autorized: true, content: [] });
-  // };
+	const verifyTurnstileToken = async (req, res) => {
+		const { turnstileToken } = req.body
+		try {
+			const response = await fetch(
+				"https://challenges.cloudflare.com/turnstile/v0/siteverify",
+				{
+					method: "POST",
+					headers: {
+						"Content-Type": "application/x-www-form-urlencoded",
+					},
+					body: new URLSearchParams({
+						secret: process.env.TURNSTILE_SECRET_KEY,
+						response: turnstileToken,
+					}).toString(),
+				},
+			)
 
-  const verifyTurnstileToken = async (req, res) => {
-    const { turnstileToken } = req.body;
-    try {
-      const response = await fetch(
-        "https://challenges.cloudflare.com/turnstile/v0/siteverify",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-          },
-          body: new URLSearchParams({
-            secret: process.env.TURNSTILE_SECRET_KEY,
-            response: turnstileToken,
-          }).toString(),
-        }
-      );
+			if (!response.ok) {
+				throw new Error(`Network response was not ok: ${response.statusText}`)
+			}
 
-      if (!response.ok) {
-        throw new Error(`Network response was not ok: ${response.statusText}`);
-      }
+			const data = await response.json()
 
-      const data = await response.json();
+			return res.status(200).send({ content: { ...data } })
+		} catch (error) {
+			console.error("Error verifying Turnstile token:", error)
+			return res
+				.status(500)
+				.send({ message: "INVALID_TOKEN", error: error.message })
+		}
+	}
 
-      return res.status(200).send({ content: { ...data } });
-    } catch (error) {
-      console.error("Error verifying Turnstile token:", error);
-      return res
-        .status(500)
-        .send({ message: "INVALID_TOKEN", error: error.message });
-    }
-  };
+	return {
+		login,
+		logout,
+		register,
+		getPublicKey,
+		verifyTurnstileToken,
+	}
+}
 
-  return {
-    login,
-    logout,
-    register,
-    getPublicKey,
-    verifyTurnstileToken,
-  };
-};
-
-export default AuthService;
+export default AuthService
